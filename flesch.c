cfebd5937ef0b1bd4a79557b2510e1a3f9843d5e
Jonah Bussard Arzadon <jarzadon@purdue.edu>
2026-03-13 02:26:01 -0400

    worked on code

--- flesch.c
+++ flesch.c
@@ -10,22 +10,75 @@
 
 int countSentences(const char* text) {
     // TODO: Implement this function
-    return 0;
+    if (text[0] == '\0') {
+        return 0;
+    }
+    int sentenceCount = 0;
+    int i = 0;
+    while (text[i] != '\0') {
+        if (text[i] == '.' || text[i] == ':' || text[i] == ';' || text[i] == '?' || text[i] == '!') {
+            sentenceCount++;
+        }
+    i++;
+    }
+    if (sentenceCount == 0) {
+        return 1;
+    }
+
+    return sentenceCount;
 }
 
 int countWords(const char* text) {
     // TODO: Implement this function
-    return 0;
+    int inWord = 0;
+    int wordCount = 0;
+    int i = 0;
+    while (text[i] != '\0') {
+        if (text[i] != ' ' && text[i] != '\t' && text[i] != '\n' && text[i] != '.' && text[i] != ':' && text[i] != ';' && text[i] != '?' && text[i] != '!') {
+            if (inWord == 0) {
+                inWord = 1;
+                wordCount++;
+            }
+        } else {
+            inWord = 0;
+        }
+        i++;
+    }
+    return wordCount;
 }
 
 int countSyllables(const char* text) {
     // TODO: Implement this function
-    return 0;
+    int inSyllable = 0;
+    int syllableCount = 0;
+    int i = 0;
+    char temp;
+    while (text[i] != '\0') {
+        temp = text[i];
+        temp = tolower(temp);
+        if (temp == 'a' || temp == 'e' || temp == 'i' || temp == 'o' || temp == 'u') {
+            if (inSyllable == 0) {
+                inSyllable = 1;
+                syllableCount++;
+            }
+        } else {
+            inSyllable = 0;
+        }
+        i++;
+    }
+    return syllableCount;
 }
 
 double calculateFleschScore(TextStats stats) {
     // TODO: Implement this function
-    return 0.0;
+    double score = 0.0;
+    if (stats.words == 0) {
+        return score;
+    }
+
+    score = 206.835 - (1.015 * ((double)stats.words / (double)stats.sentences)) - (84.6 * ((double)stats.syllables / (double)stats.words));
+
+    return score;
 }
 
 TextStats analyzeText(const char* text) {
